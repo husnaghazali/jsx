427075734a87419dbd7f07d9babe4f84b019a214
husna <husnazghazali@gmail.com>
2024-02-27 10:25:36 +0100

    error solved

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 function App() {
-    const inputType = 'number';
+    const inputType = "number";
     const minValue = 5;
 
-    return <input type={number} min={minValue} />
+    return <input type={inputType} min={minValue} />
 }
 
 root.render(<App />);
