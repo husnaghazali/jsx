b38a88647d5f4fb58e827705505a548d37e0e758
husna <husnazghazali@gmail.com>
2024-02-27 17:25:23 +0100

    import the component into another file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el);
-
-function App() {
-    
-    return <input style={{border: '3px solid blue'}} type="number" min={5} />
-}
 
 root.render(<App />);
