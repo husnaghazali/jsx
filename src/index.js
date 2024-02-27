d2b6c0f86d30426b015bac286186ce82d1dd26ef
husna <husnazghazali@gmail.com>
2024-02-27 10:12:42 +0100

    create first input with type number

--- src/index.js
+++ src/index.js
@@ -6,14 +6,8 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 function App() {
-    const name = 'Husna';
-    const age = 37;
-
-    return (
-        <h1>
-            Hi, {name} here!, I'm {age} years old.
-        </h1>
-        );
+    
+    return <input type="number" />
 }
 
 root.render(<App />);
